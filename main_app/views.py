1265cf23d15d08d4abc75983aa3dfe8361fd4672
Muguntha96 <muguntharamkumar1996@gmail.com>
2023-11-04 02:00:19 -0300

    add playlists index page

--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+class Playlist:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, title, description, genre):
+    self.title = title
+    self.description = description
+    self.genre = genre
+  
+
+playlists = [
+  Playlist('Lolo', 'tabby', 'Kinda rude.'),
+  Playlist('Sachi', 'tortoiseshell', 'Looks like a turtle.'),
+  Playlist('Fancy', 'bombay', 'Happy fluff ball.'),
+  Playlist('Bonk', 'selkirk rex', 'Meows loudly.')
+]
+    
+
 # Create your views here.
 def home(request):
   return HttpResponse('<h1>Music PlayLst</h1>')
 
 def about(request):
   return render(request,'about.html')
+
+def playlist_index(request):
+  return render(request,'playlists/index.html',{'playlists':playlists})

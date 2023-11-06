a0919f2186c366c1c7428776e6a0416d7d7f47a7
Muguntha96 <muguntharamkumar1996@gmail.com>
2023-11-06 16:44:30 -0400

    restrict the pages from the user whom are not logged in

--- soundsgood/settings.py
+++ soundsgood/settings.py
@@ -130,7 +130,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT =os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 LOGIN_REDIRECT_URL = 'song-index'
-
+LOGOUT_REDIRECT_URL = 'home'
 
 
 # Default primary key field type
